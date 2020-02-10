bd9836c23573e5f0e2ee66dc78d588975c69a849
imdanix <idanix@list.ru>
2020-02-10 15:53:25 +0300

    Update messages

--- src/main/java/me/imdanix/things/command/FailInfo.java
+++ src/main/java/me/imdanix/things/command/FailInfo.java
@@ -3,10 +3,10 @@ package me.imdanix.things.command;
 import static me.imdanix.things.utils.Utils.clr;
 
 public enum FailInfo {
-	NO_CONSOLE("&cОшибка>&f Эта команда не может быть использована из консоли."),
-	NO_PERMISSION("&cОшибка>&f У вас недостаточно прав для использования этой команды."),
-	WRONG_ARG("&cОшибка>&f Введен один или более неправильный агрумент - проверьте написание команды."),
-	OFFLINE_PLAYER("&cОшибка>&f Данного игрока нет на сервере.");
+	NO_CONSOLE("&c&lОшибка>&f Эта команда не может быть использована из консоли."),
+	NO_PERMISSION("&c&lОшибка>&f У вас недостаточно прав для использования этой команды."),
+	WRONG_ARG("&c&lОшибка>&f Введен один или более неправильный агрумент - проверьте написание команды."),
+	OFFLINE_PLAYER("&c&lОшибка>&f Данного игрока нет на сервере.");
 	private final String info;
 	FailInfo(String info) {
 		this.info=clr(info);
